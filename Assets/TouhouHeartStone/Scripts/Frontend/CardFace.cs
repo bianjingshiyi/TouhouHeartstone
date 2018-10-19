479b81cd04f4687388d59417f831bf8e68d1b570
Jiangming1399 <jiangming1399@163.com>
2018-10-19 21:33:44 +0800

    卡片的临时资源。

---
+++ Assets/TouhouHeartStone/Scripts/Frontend/CardFace.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TouhouHeartstone.Frontend
+{
+    public class CardFace : MonoBehaviour
+    {
+        [SerializeField]
+        TextMeshExtend nameLabel;
+
+        [SerializeField]
+        TextMeshExtend descLabel;
+
+        [SerializeField]
+        TextMeshExtend costLabel;
+
+        [SerializeField]
+        TextMeshExtend lifeLabel;
+
+        [SerializeField]
+        TextMeshExtend attackLabel;
+
+        [SerializeField]
+        Image headImage;
+
+        private void Start()
+        {
+            nameLabel.text = "女仆妖精";
+            descLabel.text = "嘲讽";
+
+            costLabel.text = "1";
+            lifeLabel.text = "1";
+            attackLabel.text = "1";
+        }
+    }
+}
---
+++ Assets/TouhouHeartStone/Scripts/Frontend/TwoFaceUIFix.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace TouhouHeartstone.Frontend
+{
+    [ExecuteInEditMode]
+    public class TwoFaceUIFix : MonoBehaviour
+    {
+        [SerializeField]
+        CanvasRenderer frontFace;
+
+        [SerializeField]
+        CanvasRenderer backFace;
+
+        bool front;
+
+        private void Update()
+        {
+            var r = transform.localEulerAngles.y;
+            var current = r < 90 || r > 270;
+            if (front == current) return;
+            front = current;
+
+            if (front)
+            {
+                frontFace?.SetAlpha(1);
+                backFace?.SetAlpha(0);
+            }
+            else
+            {
+                frontFace?.SetAlpha(0);
+                backFace?.SetAlpha(1);
+            }
+        }
+    }
+}
