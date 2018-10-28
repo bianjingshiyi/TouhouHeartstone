ea7260ba134f62a87d50a260e2466162c1024b3a
BJSY <bianjingshiyi@163.com>
2018-10-28 21:10:35 +0800

    正在重写游戏的后端部分。

---
+++ Assets/TouhouHeartStone/Scripts/GameContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace TouhouHeartstone
+{
+    public class GameContainer : MonoBehaviour
+    {
+        [SerializeField]
+        Backend.Game _game;
+        protected void Start()
+        {
+            _game = new Backend.Game((int)DateTime.Now.ToBinary());
+            _game.start();
+        }
+    }
+}
