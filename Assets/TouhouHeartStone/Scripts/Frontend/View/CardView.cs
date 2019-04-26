9286c5852522308d4f550e867b19fe5b24c64447
Jiangming1399 <jiangming1399@163.com>
2019-04-26 21:27:00 +0800

    微调输入逻辑？

--- Assets/TouhouHeartStone/Scripts/Frontend/View/CardView.cs
+++ Assets/TouhouHeartStone/Scripts/Frontend/View/CardView.cs
@@ -16,7 +16,7 @@ namespace TouhouHeartstone.Frontend.View
     /// <summary>
     /// 卡片的View
     /// </summary>
-    public class CardView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+    public class CardView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
     {
         #region animation_base
         Dictionary<string, ICardAnimation> cardAnimations = new Dictionary<string, ICardAnimation>();
@@ -86,6 +86,10 @@ namespace TouhouHeartstone.Frontend.View
                 throw new Exception("关联的ViewModel未找到");
 
             cardVM.OnAnimationPlay += PlayAnimation;
+
+            checker.OnClick += onMouseClick;
+            checker.OnDrag += onMouseDrag;
+            checker.OnRelease += onMouseRelease;
         }
         #region Card_state_event
         public void OnPointerEnter(PointerEventData eventData)
@@ -106,13 +110,54 @@ namespace TouhouHeartstone.Frontend.View
         public state CurrentState
         {
             get { return currentState; }
-            set { currentState = value; }
+            set
+            {
+                if (currentState != value)
+                {
+                    onStateChange(currentState, value);
+                    currentState = value;
+                }
+            }
         }
 
-        public void OnPointerDown(PointerEventData eventData)
+        void onStateChange(state original, state current)
         {
-            DebugUtils.Trace("鼠标按下");
-            switch(CurrentState) {
+
+        }
+
+        DragInputChecker checker = new DragInputChecker();
+
+        protected void Update()
+        {
+            checker.Update(Time.time);
+        }
+
+        void onMouseDrag()
+        {
+            DebugUtils.Trace("鼠标按住");
+            var deck = GetComponentInParent<Controller.UserDeckController>();
+
+            switch (CurrentState)
+            {
+                case state.hand_hover:
+                    // check,
+                    if (!deck.ThrowingCard)
+                    {
+                        // 进入跟随模式
+                        CurrentState = state.free;
+                    }
+                    break;
+                case state.center:
+                    // xxx
+                    break;
+            }
+        }
+
+        void onMouseRelease()
+        {
+            DebugUtils.Trace("鼠标松开");
+            switch (CurrentState)
+            {
                 case state.hand_hover:
                     // check,
                     break;
@@ -122,12 +167,17 @@ namespace TouhouHeartstone.Frontend.View
             }
         }
 
-        public void OnPointerUp(PointerEventData eventData)
+        public void OnPointerDown(PointerEventData eventData)
         {
-            DebugUtils.Trace("鼠标松开");
+            checker.PointerDown(Time.time);
         }
 
-        public void OnPointerClick(PointerEventData eventData)
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            checker.PointerUp();
+        }
+
+        private void onMouseClick()
         {
             DebugUtils.Trace("鼠标按下");
             var deck = GetComponentInParent<Controller.UserDeckController>();
@@ -158,6 +208,49 @@ namespace TouhouHeartstone.Frontend.View
             hand_hover,
             free,
             center
+        }
+    }
+
+    class DragInputChecker
+    {
+        public event Action OnDrag;
+        public event Action OnRelease;
+        public event Action OnClick;
+
+        /// <summary>
+        /// 上次按下时间。若为空则意味着触发或OnDrag事件了
+        /// </summary>
+        float? lastPointerDownTime;
+        const float dragThresold = 0.25f;
+
+        public void Update(float time)
+        {
+            if (lastPointerDownTime != null)
+            {
+                if (time - lastPointerDownTime > dragThresold)
+                {
+                    OnDrag?.Invoke();
+                    lastPointerDownTime = null;
+                }
+            }
+        }
+
+        public void PointerDown(float time)
+        {
+            lastPointerDownTime = time;
+        }
+
+        public void PointerUp()
+        {
+            if (lastPointerDownTime != null)
+            {
+                OnClick?.Invoke();
+                lastPointerDownTime = null;
+            }
+            else
+            {
+                OnRelease?.Invoke();
+            }
         }
     }
 }
