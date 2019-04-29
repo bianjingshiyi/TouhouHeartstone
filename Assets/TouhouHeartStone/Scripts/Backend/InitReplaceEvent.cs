91ebd2d85114edf651684244a99658f95e92e37b
BJSY <bianjingshiyi@163.com>
2019-04-29 23:07:37 +0800

    InitReplace事件的Witness新增原始卡牌RID。

--- Assets/TouhouHeartStone/Scripts/Backend/InitReplaceEvent.cs
+++ Assets/TouhouHeartStone/Scripts/Backend/InitReplaceEvent.cs
@@ -7,36 +7,38 @@ namespace TouhouHeartstone.Backend
         public InitReplaceEvent(Player player, Card[] cards) : base("onInitReplace")
         {
             this.player = player;
-            this.cards = cards;
+            originCards = cards;
         }
+        Card[] originCards { get; set; }
         public Player player { get; }
-        Card[] cards { get; set; }
         public override void execute(CardEngine engine)
         {
             //先把卡牌放回牌库
-            int[] cardsIndex = cards.Select(c => { return player["Init"].indexOf(c); }).ToArray();
-            engine.moveCard(player, "Init", cards, player, "Deck", 0);
+            int[] cardsIndex = originCards.Select(c => { return player["Init"].indexOf(c); }).ToArray();
+            engine.moveCard(player, "Init", originCards, player, "Deck", 0);
             //然后洗牌
             player["Deck"].shuffle(engine);
             //最后再抽相同数量的卡并替换
-            cards = player["Deck"][player["Deck"].count - cards.Length, player["Deck"].count - 1];
-            for (int i = 0; i < cards.Length; i++)
+            replacedCards = player["Deck"][player["Deck"].count - replacedCards.Length, player["Deck"].count - 1];
+            for (int i = 0; i < replacedCards.Length; i++)
             {
-                engine.moveCard(player, "Deck", cards[i], player, "Init", cardsIndex[i]);
+                engine.moveCard(player, "Deck", replacedCards[i], player, "Init", cardsIndex[i]);
             }
-            engine.allocateRID(cards);
+            engine.allocateRID(replacedCards);
         }
+        Card[] replacedCards { get; set; }
         public override EventWitness getWitness(CardEngine engine, Player player)
         {
             EventWitness witness = new EventWitness("onInitReplace");
             witness.setVar("playerIndex", engine.getPlayerIndex(this.player));
+            witness.setVar("originCardsRID", originCards.Select(c => { return c.getRID(); }).ToArray());
             if (player == this.player)
             {
                 //自己
-                witness.setVar("cardsDID", cards.Select(c => { return c.define.id; }).ToArray());
+                witness.setVar("replacedCardsDID", replacedCards.Select(c => { return c.define.id; }).ToArray());
             }
             //其他玩家
-            witness.setVar("cardsRID", cards.Select(c => { return c.getRID(); }).ToArray());
+            witness.setVar("replacedCardsRID", replacedCards.Select(c => { return c.getRID(); }).ToArray());
             return witness;
         }
     }
