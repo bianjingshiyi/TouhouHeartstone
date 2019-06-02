5c09a0725f3065b5b7af0b670e372cd622417773
BJSY <bianjingshiyi@163.com>
2019-06-02 22:20:44 +0800

    彻底删除了playerId。

--- Assets/TouhouHeartStone/Scripts/GameCore/IFrontend.cs
+++ Assets/TouhouHeartStone/Scripts/GameCore/IFrontend.cs
@@ -7,8 +7,6 @@ namespace TouhouHeartstone
 {
     public interface IFrontend
     {
-        [Obsolete("请使用playerIndex替代")]
-        int id { get; }
         void sendWitness(EventWitness witness);
     }
 }
