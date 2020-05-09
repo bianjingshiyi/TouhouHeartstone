52a038a73eeb625ca35e7438c6d8ce1a5c3e1fd4
BJSY <bianjingshiyi@hotmail.com>
2020-05-09 15:51:13 +0800

    疲劳UI问题修复。

--- Assets/TouhouHeartStone/Scripts/UI/Animations/FatigueAnimation.cs
+++ Assets/TouhouHeartStone/Scripts/UI/Animations/FatigueAnimation.cs
@@ -17,10 +17,12 @@ namespace UI
                 table.Fatigue.Text.text = "没有牌了，你受到了" + eventArg.player.fatigue + "点伤害！";
                 _timer.start();
             }
-            table.Fatigue.rectTransform.localScale = Vector3.Lerp(Vector3.one * .5f, Vector3.one, _timer.getProgress(0, .5f));
-            table.Fatigue.rectTransform.localPosition = Vector3.Lerp(table.SelfDeck.rectTransform.localPosition, Vector3.zero, _timer.getProgress(0, .5f));
-            table.Fatigue.alpha = 1 - _timer.getProgress(1, 1.5f);
-            table.Fatigue.Text.setAlpha(1 - _timer.getProgress(1, 1.5f));
+            table.Fatigue.rectTransform.localScale = Vector3.Lerp(Vector3.one * .5f, Vector3.one, table.Fatigue.moveCurve.Evaluate(_timer.getProgress(0, .5f)));
+            if (eventArg.player == table.player)
+                table.Fatigue.rectTransform.localPosition = Vector3.Lerp(table.SelfDeck.rectTransform.localPosition, Vector3.zero, table.Fatigue.moveCurve.Evaluate(_timer.getProgress(0, .5f)));
+            else
+                table.Fatigue.rectTransform.localPosition = Vector3.Lerp(table.EnemyDeck.rectTransform.localPosition, Vector3.zero, table.Fatigue.moveCurve.Evaluate(_timer.getProgress(0, .5f)));
+            table.Fatigue.alpha = 1 - table.Fatigue.fadeCurve.Evaluate(_timer.getProgress(1, 1.5f));
             if (!_timer.isExpired())
                 return false;
             table.Fatigue.hide();
