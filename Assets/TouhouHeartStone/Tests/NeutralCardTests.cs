a6abb0e58b8b7c0d90d2257ba01171edb70d4695
minsuika <48713512+minsuika@users.noreply.github.com>
2020-05-12 23:23:09 +0800

    圣盾的测试

--- Assets/TouhouHeartStone/Tests/NeutralCardTests.cs
+++ Assets/TouhouHeartStone/Tests/NeutralCardTests.cs
@@ -169,5 +169,34 @@ namespace Tests
             game.sortedPlayers[0].cmdAttack(game, game.sortedPlayers[0].field[0], game.sortedPlayers[1].field[0]);
             Assert.AreEqual(6, game.sortedPlayers[1].field[0].getCurrentLife());
         }
+
+        [Test]
+        public void ShieldTest()
+        {
+            THHGame game = TestGameflow.initGameWithoutPlayers(null, new GameOption()
+            {
+                shuffle = false
+            });
+            game.createPlayer(0, "玩家0", game.getCardDefine<Reimu>(), Enumerable.Repeat(game.getCardDefine<DefaultServant>() as CardDefine, 29)
+            .Concat(Enumerable.Repeat(game.getCardDefine<ShieldServant>(), 1)));
+            game.createPlayer(1, "玩家1", game.getCardDefine<Reimu>(), Enumerable.Repeat(game.getCardDefine<DefaultServant>() as CardDefine, 29)
+            .Concat(Enumerable.Repeat(game.getCardDefine<ShieldServant>(), 1)));
+            game.run();
+            game.sortedPlayers[0].cmdInitReplace(game);
+            game.sortedPlayers[1].cmdInitReplace(game);
+
+            game.sortedPlayers[0].cmdTurnEnd(game);
+            game.sortedPlayers[1].cmdUse(game, game.sortedPlayers[1].hand[1], 0);
+            game.sortedPlayers[1].cmdTurnEnd(game);
+            game.sortedPlayers[0].cmdUse(game, game.sortedPlayers[0].hand[1], 0);
+            game.sortedPlayers[0].cmdUse(game, game.sortedPlayers[0].hand[0], 1);
+            game.sortedPlayers[0].cmdTurnEnd(game);
+            game.sortedPlayers[1].cmdTurnEnd(game);
+
+            game.sortedPlayers[0].cmdAttack(game, game.sortedPlayers[0].field[0], game.sortedPlayers[1].field[0]);
+            Assert.AreEqual(6, game.sortedPlayers[0].field[0].getCurrentLife());
+            game.sortedPlayers[0].cmdAttack(game, game.sortedPlayers[0].field[1], game.sortedPlayers[1].field[0]);
+            Assert.AreEqual(1, game.sortedPlayers[0].field[1].getCurrentLife());
+        }
     }
 }
