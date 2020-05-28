c542128a23fd4f976ae94c6b0af2a66a941982e0
Jiangming1399 <jiangming1399@163.com>
2020-05-28 22:03:43 +0800

    修改网络UI

--- Assets/TouhouHeartStone/Scripts/UI/Networking/NetworkingPage.cs
+++ Assets/TouhouHeartStone/Scripts/UI/Networking/NetworkingPage.cs
@@ -26,7 +26,7 @@ namespace UI
         private void createRemoteRoom()
         {
             // todo: 向服务器发送房间信息
-            StatusText.text = "正在服务器上创建游戏房间";
+            StatusText.text = "服务器上创建游戏房间\n127.0.0.1:" + Networking.host.port;
             Networking.CreateRoom();
 
             RemoteRoomScrollView.RoomList.AddItem("测试本机", "127.0.0.1");
@@ -34,8 +34,8 @@ namespace UI
 
         private void createLocalRoom()
         {
-            StatusText.text = "本地局域网上创建游戏房间";
             Networking.CreateRoom();
+            StatusText.text = "本地局域网上创建游戏房间\n127.0.0.1:" + Networking.host.port;
 
             LocalRoomScrollView.RoomList.AddItem("测试本机", "127.0.0.1");
         }
