6b32e08bc2bc0a60d2cf138c8eb3dae4c6ee20ab
Jiangming1399 <jiangming1399@163.com>
2020-06-02 21:40:14 +0800

    添加Excel到DataSet转换
    添加转换的方法、配置项目和对应打包钩子
    为卡片读取添加DataSet的读取方法
    为ResourceManager添加DataSet的读取方法

--- Assets/TouhouHeartStone/Scripts/ResourceManager.cs
+++ Assets/TouhouHeartStone/Scripts/ResourceManager.cs
@@ -6,14 +6,17 @@ using UnityEngine.Networking;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System;
+using System.Data;
+using System.Runtime.Serialization.Formatters.Binary;
+
 namespace Game
 {
     public class ResourceManager : Manager
     {
         public Task<Workbook> loadExcel(string path, RuntimePlatform? platform = null)
         {
-            if (path == null)
-                throw new System.Exception("路径不能为空");
+            if (string.IsNullOrEmpty(path))
+                return null;
             platform = getPlatform(platform);
             switch (platform)
             {
@@ -25,8 +28,8 @@ namespace Game
         }
         public Task<Texture2D> loadTexture(string path, RuntimePlatform? platform = null)
         {
-            if (path == null)
-                throw new System.Exception("路径不能为空");
+            if (string.IsNullOrEmpty(path))
+                return null;
             platform = getPlatform(platform);
             switch (platform)
             {
@@ -36,6 +39,20 @@ namespace Game
                     return loadTextureBySystemIOWithFallback(path);
             }
         }
+        public Task<DataSet> loadDataSet(string path, RuntimePlatform? platform = null)
+        {
+            if (string.IsNullOrEmpty(path))
+                return null;
+            platform = getPlatform(platform);
+            switch (platform)
+            {
+                case RuntimePlatform.Android:
+                    return loadDataSetByWebRequest(path);
+                default:
+                    return loadDataSetBySystemIO(path);
+            }
+        }
+
         public async Task<Texture2D> loadTextureByWebRequestWithFallback(string path)
         {
             var task = loadTextureByWebRequest(path);
@@ -122,12 +139,29 @@ namespace Game
                 return Task.FromResult(Workbook.Load(stream));
             }
         }
+        public Task<DataSet> loadDataSetBySystemIO(string path)
+        {
+            using (FileStream stream = getFileStream(path))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                return Task.FromResult(bf.Deserialize(stream) as DataSet);
+            }
+        }
         private async Task<Workbook> loadExcelByWebRequest(string path)
         {
             byte[] data = await loadBytesByWebRequest(path);
             using (MemoryStream stream = new MemoryStream(data))
             {
                 return Workbook.Load(stream);
+            }
+        }
+        private async Task<DataSet> loadDataSetByWebRequest(string path)
+        {
+            byte[] data = await loadBytesByWebRequest(path);
+            using (MemoryStream stream = new MemoryStream(data))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                return bf.Deserialize(stream) as DataSet;
             }
         }
         private async Task<byte[]> loadBytesBySystemIO(string path)
