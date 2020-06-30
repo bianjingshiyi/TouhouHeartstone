6cf45f226491913997c41c54ff78a4b5ed2a1e95
Jiangming1399 <jiangming1399@163.com>
2020-06-30 20:17:33 +0800

    为排序添加额外的选项

--- Assets/TouhouHeartStone/Scripts/UI/Builds/FilterPanel.cs
+++ Assets/TouhouHeartStone/Scripts/UI/Builds/FilterPanel.cs
@@ -20,6 +20,17 @@ namespace UI
             new KeyValuePair<string, string>("Skill", "技能"),
             new KeyValuePair<string, string>("Spell", "法术")
         };
+        enum SortMethod
+        {
+            ID,
+            ID_R,
+            Cost,
+            Cost_R,
+            Attack,
+            Attack_R
+        };
+
+        string[] sortMethodsName = { "默认", "倒序", "费用升序", "费用降序", "攻击升序", "攻击降序" };
 
         string filterKeyword = "";
         string filterType = "";
@@ -27,7 +38,7 @@ namespace UI
         int filterRare = 0;
         int filterCharacter = 0;
 
-        int sortMethod = 0;
+        SortMethod sortMethod = 0;
 
         partial void onAwake()
         {
@@ -39,11 +50,20 @@ namespace UI
 
             DropdownSort.onValueChanged.AddListener(onSortMethodChange);
 
+            // 填充过滤类型
             DropdownType.options.Clear();
             for (int i = 0; i < typeName.Length; i++)
             {
                 DropdownType.options.Add(new UnityEngine.UI.Dropdown.OptionData(typeName[i].Value));
             }
+
+            // 填充排序方式
+            DropdownSort.options.Clear();
+            for (int i = 0; i < sortMethodsName.Length; i++)
+            {
+                DropdownSort.options.Add(new UnityEngine.UI.Dropdown.OptionData(sortMethodsName[i]));
+            }
+
             // todo: 填充角色列表
 
         }
@@ -126,18 +146,25 @@ namespace UI
             switch (sortMethod)
             {
                 default:
-                case 0:
+                case SortMethod.ID:
                     return a.card.id - b.card.id;
-                case 1:
+                case SortMethod.ID_R:
+                    return b.card.id - a.card.id;
+                case SortMethod.Cost:
                     return a.card.getCost() - b.card.getCost();
-                case 2:
+                case SortMethod.Attack:
                     return a.card.getAttack() - b.card.getAttack();
+                case SortMethod.Cost_R:
+                    return b.card.getCost() - a.card.getCost();
+                case SortMethod.Attack_R:
+                    return b.card.getAttack() - a.card.getAttack();
             }
         }
 
         void onSortMethodChange(int id)
         {
-            sortMethod = id;
+            UberDebug.Log($"Sort method change: {id}");
+            sortMethod = (SortMethod)id;
 
             onSortChange?.Invoke();
         }
