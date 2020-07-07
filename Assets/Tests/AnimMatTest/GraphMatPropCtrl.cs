5af2d2d2c87052f16f47ac64c16a899460f66fbc
BJSY <bianjingshiyi@hotmail.com>
2020-07-07 14:45:55 +0800

    动画控制材质参数。

---
+++ Assets/Tests/AnimMatTest/GraphMatPropCtrl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BJSYGameCore.Animations
+{
+    [RequireComponent(typeof(Graphic))]
+    public abstract class GraphMatPropCtrl : MatPropCtrl
+    {
+        Graphic _graphic;
+        public Graphic graphic
+        {
+            get
+            {
+                if (_graphic == null)
+                    _graphic = GetComponent<Graphic>();
+                return _graphic;
+            }
+        }
+        public override Material material
+        {
+            get { return graphic.material; }
+            set { graphic.material = value; }
+        }
+    }
+}
---
+++ Assets/TouhouHeartStone/Scripts/Animations/MatCtrl/ServantMatCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using BJSYGameCore.Animations;
+namespace Game
+{
+    public class ServantMatCtrl : GraphMatPropCtrl
+    {
+        [SerializeField]
+        float _Gray;
+        [SerializeField]
+        Color _Color;
+        protected override void Update()
+        {
+            material.SetFloat(nameof(_Gray), _Gray);
+            material.SetColor(nameof(_Color), _Color);
+        }
+        private void Reset()
+        {
+            _Gray = material.GetFloat(nameof(_Gray));
+            _Color = material.GetColor(nameof(_Color));
+        }
+    }
+}
