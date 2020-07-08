232d17c5472a6f8ed6e09cbba59860514385abcb
BJSY <bianjingshiyi@hotmail.com>
2020-07-08 19:22:59 +0800

    新增粒子测试。

---
+++ Assets/Tests/ParticleTest/ParticleToCanvas.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleToCanvas : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _particle;
+    [SerializeField]
+    RectTransform _from;
+    [SerializeField]
+    RectTransform _to;
+    //尝试做点什么，让粒子能从_from移动到_to，并且能在Canvas上渲染出来！
+}
