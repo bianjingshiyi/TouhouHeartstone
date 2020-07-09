d9885811090f83a61ee81d2211feba7a3efc4f2c
Jim Jiang <jim@lotlab.org>
2020-07-09 21:34:25 +0800

    完成ParticalToCanvas动画

--- Assets/Tests/ParticleTest/ParticleToCanvas.cs
+++ Assets/Tests/ParticleTest/ParticleToCanvas.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class ParticleToCanvas : MonoBehaviour
 {
     [SerializeField]
-    GameObject _particle;
+    RectTransform _particle;
     [SerializeField]
     RectTransform _from;
     [SerializeField]
     RectTransform _to;
     //尝试做点什么，让粒子能从_from移动到_to，并且能在Canvas上渲染出来！
+
+    [SerializeField]
+    float step = 0.01f;
+
+    bool dir = false;
+
+    float Step => dir ? step : -step;
+
+    float val = 0;
+
+    private void Update()
+    {
+        if (val <= 0) dir = true;
+        if (val >= 1) dir = false;
+
+        val += Step;
+
+        _particle.position = Vector3.Slerp(_from.position, _to.position, val);
+    }
 }
