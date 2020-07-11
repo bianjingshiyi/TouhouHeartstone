e8cfd6f8f06ec6142089cca217b25b46aa902e4c
233huang <1900675683@qq.com>
2020-07-11 11:18:54 +0800

    帕秋莉卡牌的实现

--- Assets/TouhouHeartStone/Scripts/GameCore/Builtin/Patchouli.cs
+++ Assets/TouhouHeartStone/Scripts/GameCore/Builtin/Patchouli.cs
@@ -79,23 +79,139 @@ namespace TouhouHeartstone.Builtin
                 return false;
             },async (game,card,arg,targets)=>
             {
-
+                new CostFixer().onEnable(game,card);
             })
         };
+        class CostFixer : PassiveEffect
+        {
+            public override string[] piles { get; } = new string[0];
+            Trigger<THHPlayer.UseEventArg> UseTrigger { get; set; } = null;
+            Trigger<THHGame.TurnEndEventArg> TurnEndTrigger { get; set; } = null;
+            Buff buff = new GeneratedBuff(ID, new CostModifier(-5));
+            Dictionary<Card, Buff> buffDic { get; } = new Dictionary<Card, Buff>();
+            public override void onEnable(THHGame game, Card card)
+            {
+                foreach (var target in card.getOwner().hand.Where(c => c.define is SpellCardDefine))
+                {
+                    target.addBuff(game, buff);
+                    buffDic.Add(target, buff);
+                }
+                if (UseTrigger == null)
+                {
+                    UseTrigger = new Trigger<THHPlayer.UseEventArg>(arg =>
+                    {
+                        if (arg.card != card)
+                        {
+                            if (arg.card.define is SpellCardDefine)
+                                onDisable(game, card);
+                        }
+                        return Task.CompletedTask;
+                    });
+                    game.triggers.registerAfter(UseTrigger);
+                }
+                if (TurnEndTrigger == null)
+                {
+                    TurnEndTrigger = new Trigger<THHGame.TurnEndEventArg>(arg =>
+                    {
+                        if (arg.player == card.getOwner())
+                            onDisable(game, card);
+                        return Task.CompletedTask;
+                    });
+                    game.triggers.registerAfter(TurnEndTrigger);
+                }
+            }
+            public override void onDisable(THHGame game, Card card)
+            {
+                if (UseTrigger != null)
+                {
+                    game.triggers.removeAfter(UseTrigger);
+                    UseTrigger = null;
+                }
+                if (TurnEndTrigger != null)
+                {
+                    game.triggers.removeAfter(TurnEndTrigger);
+                    TurnEndTrigger = null;
+                }
+                foreach (var pair in buffDic)
+                {
+                    pair.Key.removeBuff(game, pair.Value);
+                }
+            }
+        }
     }
     /// <summary>
-    /// 777 魔神机甲 如果你在本回合中使用过花费大于0的法术牌，花费-2
+    /// 777 帕秋莉机器人 如果你在本回合中使用过花费大于0的法术牌，花费-2
     /// </summary>
-    [Obsolete]
     public class MegaReaver : ServantCardDefine
     {
         public const int ID = Patchouli.ID | CardCategory.SERVANT | 0x005;
         public override int id { get; set; } = ID;
-        public override int cost { get; set; } = 4;
-        public override int attack { get; set; } = 8;
-        public override int life { get; set; } = 8;
+        public override int cost { get; set; } = 7;
+        public override int attack { get; set; } = 7;
+        public override int life { get; set; } = 7;
         public override string[] keywords { get; set; } = new string[0];
-        public override IEffect[] effects { get; set; } = new IEffect[0];
+        public override IEffect[] effects { get; set; } = new IEffect[]
+        {
+            new CostFixer()
+        };
+        class CostFixer : PassiveEffect
+        {
+            public override string[] piles { get; } = new string[] { PileName.HAND };
+            CostModifier _modifier = new CostModifier();
+            Trigger<THHPlayer.UseEventArg> onCardEnterHandTrigger { get; set; } = null;
+            Trigger<THHGame.TurnEndEventArg> TurnEndTrigger { get; set; } = null;
+            public override void onEnable(THHGame game, Card card)
+            {
+                if (onCardEnterHandTrigger == null)
+                {
+                    onCardEnterHandTrigger = new Trigger<THHPlayer.UseEventArg>(arg =>
+                    {
+                        if (arg.card.define is SpellCardDefine && arg.card.getCost() > 0)
+                        {
+                            if (card.owner == game.currentPlayer)
+                                card.addModifier(game, _modifier);
+                        }
+                        return Task.CompletedTask;
+                    });
+                    game.triggers.registerAfter(onCardEnterHandTrigger);
+                }
+                if (TurnEndTrigger == null)
+                {
+                    TurnEndTrigger = new Trigger<THHGame.TurnEndEventArg>(arg =>
+                    {
+                        card.removeModifier(game, _modifier);
+                        return Task.CompletedTask;
+                    });
+                    game.triggers.registerAfter(TurnEndTrigger);
+                }
+            }
+            public override void onDisable(THHGame game, Card card)
+            {
+                if (onCardEnterHandTrigger != null)
+                {
+                    game.triggers.removeAfter(onCardEnterHandTrigger);
+                    onCardEnterHandTrigger = null;
+                }
+                if (TurnEndTrigger != null)
+                {
+                    game.triggers.removeAfter(TurnEndTrigger);
+                    TurnEndTrigger = null;
+                }
+                card.removeModifier(game, _modifier);
+            }
+            class CostModifier : PropModifier<int>
+            {
+                public override string propName { get; } = nameof(ServantCardDefine.cost);
+                public override int calc(Card card, int value)
+                {
+                    return value - 2;
+                }
+                public override PropModifier clone()
+                {
+                    return new CostModifier();
+                }
+            }
+        }
     }
     /// <summary>
     /// 4 贤者之石 05 每当你使用一张法术，将一张随机元素法术置入你的手牌并使耐久-1。战吼：将一张随机元素法术置入你的手牌。
@@ -187,7 +303,7 @@ namespace TouhouHeartstone.Builtin
         };
     }
     /// <summary>
-    /// 巨石 衍生物
+    /// 巨石 衍生物，无法攻击
     /// </summary>
     public class Boulder : ServantCardDefine
     {
@@ -200,7 +316,7 @@ namespace TouhouHeartstone.Builtin
         public override string[] keywords { get; set; } = new string[] { Keyword.TAUNT };
         public override IEffect[] effects { get; set; } = new IEffect[]
         {
-            new THHEffectBefore<THHGame.TurnStartEventArg>(PileName.NONE,(game,card,arg)=>
+            new THHEffectAfter<THHGame.TurnStartEventArg>(PileName.FIELD,(game,card,arg)=>
             {
                 return true;
             },(game,card,targets)=>
@@ -300,6 +416,7 @@ namespace TouhouHeartstone.Builtin
                foreach(Card target in game.getOpponent(card.owner as THHPlayer).field)
                 {
                     target.define.effects = new IEffect[0];
+                    await target.die(game);
                 }
             })
         };
@@ -310,10 +427,25 @@ namespace TouhouHeartstone.Builtin
     public class ElementSprite : ServantCardDefine
     {
         public const int ID = Patchouli.ID | CardCategory.SPELL | 0x015;
-        public override int cost { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override int attack { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override int life { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override int id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int id { get; set; } = ID;
+        public override int cost { get; set; } = 2;
+        public override int attack { get; set; } = 3;
+        public override int life { get; set; } = 2;
+        public override IEffect[] effects { get; set; } = new IEffect[]
+        {
+            new NoTargetEffect(effect)
+        };
+        static async Task effect(THHGame game, Card card)
+        {
+            int[] ids = new int[] { SylphyHorn.ID, AgniShine.ID, PrincessUndine.ID, MetalFatigue.ID, TrilithonShake.ID };
+            Card spellcard = game.createCardById(ids[game.randomInt(0, ids.Length - 1)]);
+
+            if (!card.getOwner().hand.isFull)
+            {
+                await card.getOwner().hand.add(game, spellcard);
+                spellcard.addBuff(game, new GeneratedBuff(ID, new CostModifier(0, true)));
+            }
+        }
     }
     /// <summary>
     /// 2 大图书馆 发现一张牌库中的法术，抽这张牌并使其花费-1
@@ -321,8 +453,20 @@ namespace TouhouHeartstone.Builtin
     public class TheGreatLibrary : SpellCardDefine
     {
         public const int ID = Patchouli.ID | CardCategory.SPELL | 0x016;
-        public override int cost { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override int id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int id { get; set; } = ID;
+        public override int cost { get; set; } = 2;
+        public override IEffect[] effects { get; set; } = new IEffect[] {
+            new NoTargetEffect(effect)
+        };
+        static async Task effect(THHGame game, Card card)
+        {
+            var discovered = await card.getOwner().discover(game, card.getOwner().deck.Where(c => c.isSpell()));
+            if (!card.getOwner().hand.isFull)
+            {
+                await card.getOwner().deck.moveTo(game, discovered, card.getOwner().hand);
+                discovered.addBuff(game, new GeneratedBuff(ID, new CostModifier(-1)));
+            }
+        }
     }
     /// <summary>
     /// 331 图书馆保卫者 战吼：你手牌中每有一张法术牌，便获得+1生命值
@@ -330,10 +474,18 @@ namespace TouhouHeartstone.Builtin
     public class LibraryProtector : ServantCardDefine
     {
         public const int ID = Patchouli.ID | CardCategory.SPELL | 0x017;
-        public override int cost { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override int attack { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override int life { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override int id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int id { get; set; } = ID;
+        public override int cost { get; set; } = 3;
+        public override int attack { get; set; } = 3;
+        public override int life { get; set; } = 1;
+        public override IEffect[] effects { get; set; } = new IEffect[]
+        {
+            new NoTargetEffect(effect)
+        };
+        static async Task effect(THHGame game, Card card)
+        {
+            card.addBuff(game, new GeneratedBuff(ID, new LifeModifier(card.getOwner().hand.Where(c => c.isSpell()).Count()-1)));
+        }
     }
     /// <summary>
     /// 4 奥术知识 抽两张法术牌并使其花费-1
@@ -341,7 +493,24 @@ namespace TouhouHeartstone.Builtin
     public class ArcaneKnowledge : SpellCardDefine
     {
         public const int ID = Patchouli.ID | CardCategory.SPELL | 0x018;
-        public override int cost { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override int id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int id { get; set; } = ID;
+        public override int cost { get; set; } = 4;
+        public override IEffect[] effects { get; set; } = new IEffect[] {
+            new NoTargetEffect(effect)
+        };
+        static async Task effect(THHGame game, Card card)
+        {
+            await game.triggers.doEvent(new THHPlayer.DrawEventArg() { player = card.getOwner() }, arg =>
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    arg.card = arg.player.deck.Where(c => c.isSpell()).First();
+                    arg.player.deck.moveTo(game, arg.card, arg.player.hand, arg.player.hand.count);
+                    game.logger.log(arg.player + "抽" + arg.card);
+                    arg.card.addBuff(game, new GeneratedBuff(ID, new CostModifier(-1)));
+                }
+                return Task.CompletedTask;
+            });
+        }
     }
 }
