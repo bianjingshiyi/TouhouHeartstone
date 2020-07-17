65e7338b0a8a8945b4736e03a1754ec067cb03ea
BJSY <bianjingshiyi@163.com>
2020-07-17 23:38:39 +0800

    这次真的修好了

--- Assets/TouhouHeartStone/Tests/CardSystemTests.cs
+++ Assets/TouhouHeartStone/Tests/CardSystemTests.cs
@@ -7,6 +7,8 @@ using System;
 using TouhouHeartstone;
 using TouhouHeartstone.Builtin;
 using TouhouCardEngine;
+using TouhouCardEngine.Interfaces;
+
 namespace Tests
 {
     public class CardSystemTests
@@ -192,6 +194,7 @@ namespace Tests
         {
             public override int id { get; } = 0;
             public override PropModifier[] modifiers { get; } = null;
+            public override IPassiveEffect[] effects { get; }
             public TestBuff(params PropModifier[] modifiers)
             {
                 this.modifiers = modifiers;
