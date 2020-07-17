35e5cc29c4fa72a73f8237574a31e2243f96b605
BJSY <bianjingshiyi@163.com>
2020-07-18 00:50:08 +0800

    随从属性接口实现。

--- Assets/TouhouHeartStone/Scripts/Animations/AddModiAnim.cs
+++ Assets/TouhouHeartStone/Scripts/Animations/AddModiAnim.cs
@@ -1,4 +1,6 @@
 ﻿using UI;
+using UnityEngine;
+using TouhouHeartstone;
 namespace Game
 {
     class AddModiAnim : EventAnimation<TouhouCardEngine.Card.AddModiEventArg>
@@ -6,18 +8,35 @@ namespace Game
         AnimAnim _anim;
         public override bool update(TableManager table, TouhouCardEngine.Card.AddModiEventArg eventArg)
         {
-            //if (table.tryGetServant(eventArg.card, out var servant))
-            //{
-            //    servant.onAddModi.beforeAnim.Invoke();
-            //    if (!string.IsNullOrEmpty(servant.onAddModi.animName))
-            //    {
-            //        if (_anim == null)
-            //            _anim = new AnimAnim(servant.animator, servant.onAddModi.animName);
-            //        if (!_anim.update(table))
-            //            return false;
-            //    }
-            //    servant.onAddModi.afterAnim.Invoke();
-            //}
+            SimpleAnim simpleAnim = null;
+            Animator animator = null;
+            if (table.tryGetServant(eventArg.card, out var servant))
+            {
+                animator = servant.animator;
+                if (eventArg.modifier is AttackModifier atkMod)
+                {
+                    if (atkMod.value > 0)
+                        simpleAnim = servant.onAttackUp;
+                    else
+                        simpleAnim = servant.onAttackDown;
+                }
+                else if (eventArg.modifier is LifeModifier lifMod)
+                {
+                    if (lifMod.value > 0)
+                        simpleAnim = servant.onLifeUp;
+                    else
+                        simpleAnim = servant.onLifeDown;
+                }
+            }
+            simpleAnim.beforeAnim.Invoke();
+            if (!string.IsNullOrEmpty(simpleAnim.animName))
+            {
+                if (_anim == null)
+                    _anim = new AnimAnim(animator, simpleAnim.animName);
+                if (!_anim.update(table))
+                    return false;
+            }
+            simpleAnim.afterAnim.Invoke();
             return true;
         }
     }
