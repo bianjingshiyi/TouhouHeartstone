e9111fc428ec600804911828abd685b1eb72d74e
BJSY <bianjingshiyi@163.com>
2020-07-18 16:41:34 +0800

    清理了伤害动画。

--- Assets/TouhouHeartStone/Scripts/GameCore/PropModifiers/CostModifier.cs
+++ Assets/TouhouHeartStone/Scripts/GameCore/PropModifiers/CostModifier.cs
@@ -10,12 +10,15 @@ namespace TouhouHeartstone
         public CostModifier(int value, bool isSet) : base(nameof(ServantCardDefine.cost), value, isSet)
         {
         }
-        //public CostModifier(int value,) : base(nameof(ServantCardDefine.cost), value)
-        //{
-
-        //}
+        public CostModifier(int value, CheckConditionDelegate onCheckCondition) : base(nameof(ServantCardDefine.cost), value)
+        {
+            _onCheckCondition = onCheckCondition;
+        }
+        CheckConditionDelegate _onCheckCondition;
         public override bool checkCondition(IGame game, Card card)
         {
+            if (_onCheckCondition != null && !_onCheckCondition(game as THHGame, card))
+                return false;
             return base.checkCondition(game, card);
         }
         protected CostModifier(IntPropModifier origin) : base(origin)
